0c35fa471531222a9ca1a3a776829b5b0fc45867
cheeseBurger321 <100348695+cheeseBurger321@users.noreply.github.com>
2022-08-20 22:26:11 +1000

    posgafhsdkjsf

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { PublicKey } from '@solana/web3.js';
+import React, { useEffect, useState } from 'react';
+import Product from "../components/Product";
+
 import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
@@ -7,8 +8,21 @@ import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 
 const App = () => {
-  // This will fetch the users' public key (wallet address) from any wallet we support
   const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+
 
   const renderNotConnectedContainer = () => (
     <div>
@@ -17,6 +31,14 @@ const App = () => {
       <div className="button-container">
         <WalletMultiButton className="cta-button connect-wallet-button" />
       </div>    
+    </div>
+  );
+  
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
     </div>
   );
 
@@ -29,10 +51,9 @@ const App = () => {
         </header>
 
         <main>
-          {/* We only render the connect button if public key doesn't exist */}
-          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
-
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
+
 
         
       </div>
